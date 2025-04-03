795d9b74e6a4ce168dfef89527cacdae24093a30
John Keno Iweh <iwehjohn@gmail.com>
2025-04-03 23:41:35 +0000

    connected mogodb & schema

--- myApp.js
+++ myApp.js
@@ -1,7 +1,27 @@
+let mongoose = require('mongoose');
 require('dotenv').config();
 
 
-let Person;
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB successfully");
+});
+
+
+const PersonSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  favoriteFoods: [String]
+
+});
+let Person = mongoose.model("Person",PersonSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
